b280554e208bd674f381d4d46c57291ed48683cc
lech-qr <lech@jet-it.pl>
2024-02-07 11:13:02 +0100

    Koniec js

--- js/script.js
+++ js/script.js
@@ -10,6 +10,12 @@
         tlusta: 0,
         mieszana: 0
     }
+    let option = []
+
+    // Typ cery
+    type = 'nieznany'
+    // Ilość kosmetyków
+    let howMany = 'nieznana'
 
     // Czas zmiany w ms
     const time = 700
@@ -20,13 +26,17 @@
     const close = document.querySelector('nav .close')
     const pagination = document.querySelector('.pagination > p')
 
-    const button = document.querySelectorAll('.ank_btn')
+    const buttons = document.querySelectorAll('.ank_btn')
     const btnSucha = document.querySelectorAll('[data-id="sucha"]')
     const btnNormalna = document.querySelectorAll('[data-id="normalna"]')
     const btnTlusta = document.querySelectorAll('[data-id="tlusta"]')
     const btnMieszana = document.querySelectorAll('[data-id="mieszana"]')
 
-    const resultList = document.querySelectorAll('.result li')
+    const getResult = document.querySelectorAll('button.result');
+    const resultH2 = document.querySelector('section.slide08 h2');
+    const resultPe = document.querySelector('section.slide08 p');
+    const resultA1 = document.querySelector('section.slide08 a:nth-of-type(1)');
+    const resultA2 = document.querySelector('section.slide08 a:nth-of-type(2)');
 
     // Funkcje
     // Ukryj slajd
@@ -47,19 +57,23 @@
     // Wybór
     const suchaOpt = () => {
         result['sucha'] = result['sucha'] + 1
-        console.log(result);
+        option.push('sucha')
+        console.log(result, option)
     }
     const normalnaOpt = () => {
         result['normalna'] = result['normalna'] + 1
-        console.log(result);
+        option.push('normalna')
+        console.log(result, option)
     }
     const tlustaOpt = () => {
         result['tlusta'] = result['tlusta'] + 1
-        console.log(result);
+        option.push('tlusta')
+        console.log(result, option)
     }
     const mieszanaOpt = () => {
         result['mieszana'] = result['mieszana'] + 1
-        console.log(result);
+        option.push('mieszana')
+        console.log(result, option)
     }
 
     // Następny slajd
@@ -89,6 +103,7 @@
                     showSlide()
                 }
             } else {
+                // Podstawowa obsługa przejścia do kolejnego slajdu
                 indexUp()
                 // Wypisz index w paginacji
                 pagination.textContent = sIndex
@@ -103,8 +118,25 @@
         }, time)
     }
     // Powrót do poprzedniego slajdu
+    const subtract = () => {
+        if (option[option.length - 1] == 'sucha') {
+            result['sucha'] = result['sucha'] - 1
+            option.pop()
+        } else if (option[option.length - 1] == 'normalna') {
+            result['normalna'] = result['normalna'] - 1
+            option.pop()
+        } else if (option[option.length - 1] == 'tlusta') {
+            result['tlusta'] = result['tlusta'] - 1
+            option.pop()
+        } else if (option[option.length - 1] == 'mieszana') {
+            result['mieszana'] = result['mieszana'] - 1
+            option.pop()
+        }
+        console.log(result, option)
+    }
     const prevSlide = () => {
         hideSlide()
+        subtract()
         setTimeout(() => {
             article.classList.remove("slide0" + sIndex)
             slide = slide - 1
@@ -119,14 +151,14 @@
     // Ustal typ cery
     const findGreatest = (result) => {
         let greatest = result['sucha']
-        let type = 'sucha'
+        type = 'sucha'
         if (result['normalna'] > greatest) {
             greatest = result['normalna'];
             type = 'normalna'
         }
         if (result['tlusta'] > greatest) {
             greatest = result['tlusta'];
-            type = 'tłusta'
+            type = 'tlusta'
         }
         if (result['mieszana'] > greatest) {
             greatest = result['mieszana'];
@@ -140,6 +172,7 @@
         // Jeśli start jest wywoływany podczas wypełniania ankiety (button close)
         hideSlide()
         article.classList.remove("active", "slide0" + sIndex);
+        option = []
         result = {
             sucha: 0,
             normalna: 0,
@@ -165,7 +198,7 @@
     // Nav > prev
     prev.addEventListener("click", prevSlide);
     // Slide 00
-    button[0].addEventListener("click", nextSlide);
+    buttons[0].addEventListener("click", nextSlide);
     // Slide od 01 do 05
     // Sucha
     btnSucha.forEach(button => {
@@ -195,5 +228,60 @@
             nextSlide()
         });
     })
+    // Losuj kolejność pytań
+    buttons.forEach(function (element) {
+        let randomNumber = Math.floor(Math.random() * 100) + 1; // Random number between 1 and 100
+        // element.style.order = randomNumber;
+    });
 
+    // Generowanie wyniku
+    const prntResult = () => {
+        console.log(`Typ cery to ${type}, a ilość kroków to ${howMany}`);
+        if (type === 'sucha') {
+            resultPe.textContent = 'Twoja skóra wymaga dogłębnego nawilżenia. W kosmetykach oprócz składników typowo nawilżających szukaj również takich, które ograniczą TEWL (transepidermalą utratę wody) i odbudują barierę hydrolipidową Twojej cery. Nie zapominaj także o ochronie przeciwsłonecznej i stosowaniu składników anti-aging, ponieważ Twoja skóra stosunkowo szybko ulega procesom starzenia.'
+        } else if (type === 'normalna') {
+            resultPe.textContent = 'Twoja skóra jest w dobrej kondycji, prawda? Mimo to, pamiętaj o odpowiedniej pielęgnacji. Wybieraj kosmetyki, które ochronią Twoją cerę przed negatywnym wpływem wolnych rodników, promieniowania UV i innych czynników zewnętrznych. Dzięki odpowiedniej pielęgnacji będziesz latami cieszyć się piękną i zdrową cerą.'
+        } else if (type === 'tlusta') {
+            resultPe.textContent = 'Twoja skóra wytwarza nadmiar sebum na całej powierzchni twarzy. Mimo to, może być jednocześnie przesuszona. Intensywnie wysuszające kosmetyki, które mają pomóc Ci doraźnie zmatowić cerę, w perspektywie czasu doprowadzają do jeszcze większej produkcji sebum oraz przesuszenia. W swojej pielęgnacji, zamiast składników agresywnie wysuszających postaw na te, które nawilżą skórę oraz zadziałają seboregulująco.'
+        } else if (type === 'mieszana') {
+            resultPe.textContent = 'Twoja skóra wytwarza nadmiar sebum w strefie T (czoło, nos, broda). Pozostałe partie (policzki, skronie, okolica oczu i ust) mogą być normalne lub przesuszone. Twoja pielęgnacja musi być wielokierunkowa. Na całą powierzchnię twarzy stosuj kosmetyki nawilżające i odbudowujące barierę hydrolipidową skóry, natomiast w strefie T stosuj dodatkowo produkty seboregulujące. Pamiętaj, że intensywnie wysuszające kosmetyki, które mają pomóc Ci doraźnie zmatowić cerę, w perspektywie czasu doprowadzają do jeszcze większej produkcji sebum i silnego przesuszenia.'
+        }
+        // Pokaż slide z wynikiem
+        article.classList.add(type)
+        resultH2.textContent = 'Cera ' + type
+        resultA1.href = advice[type]
+        // resultA2.href = list[type]
+        nextSlide()
+    }
+
+    getResult[0].addEventListener("click", function () {
+        howMany = 'do 3'
+        advice = {
+            sucha: '/Cera-Sucha-poznaj-porade-Eksperta-3-ccms-pol-129.html',
+            normalna: '/Cera-Normalna-poznaj-porade-Eksperta-3-ccms-pol-138.html',
+            tlusta: '/Cera-Tlusta-poznaj-porade-Eksperta-3-ccms-pol-132.html',
+            mieszana: '/Cera-Mieszana-poznaj-porade-Eksperta-3-ccms-pol-135.html'
+        }
+        prntResult()
+    })
+    getResult[1].addEventListener("click", function () {
+        howMany = 'między 3 a 6'
+        advice = {
+            sucha: '/Cera-Sucha-poznaj-porade-Eksperta-6-ccms-pol-130.html',
+            normalna: '/Cera-Normalna-poznaj-porade-Eksperta-6-ccms-pol-139.html',
+            tlusta: '/Cera-Tlusta-poznaj-porade-Eksperta-6-ccms-pol-133.html',
+            mieszana: '/Cera-Mieszana-poznaj-porade-Eksperta-6-ccms-pol-136.html'
+        }
+        prntResult()
+    })
+    getResult[2].addEventListener("click", function () {
+        howMany = '6 i więcej'
+        advice = {
+            sucha: '/Cera-Sucha-poznaj-porade-Eksperta-9-ccms-pol-131.html',
+            normalna: '/Cera-Normalna-poznaj-porade-Eksperta-9-ccms-pol-140.html',
+            tlusta: '/Cera-Tlusta-poznaj-porade-Eksperta-9-ccms-pol-134.html',
+            mieszana: '/Cera-Mieszana-poznaj-porade-Eksperta-9-ccms-pol-137.html'
+        }
+        prntResult()
+    })
 });
